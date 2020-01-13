8ba68f21ba35befcfeb102e87188a2d4e09e5883
ajayda24 <59312969+ajayda24@users.noreply.github.com>
2020-01-13 22:43:26 +0530

    Add files via upload

---
+++ gci.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "GCI Username --> Ajay Daniel Trevor\n";
+  cout<<"I completed so many task in GCI and I will try to do more!!\n";
+  return 0;
+}
